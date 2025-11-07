39c061548d26c930e8bd97c7c5ac96f3bcfbb610
Andrew <andrew8230@gmail.com>
2025-11-06 18:42:42 -0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   // Update base to match the repo name for GitHub Pages
-  base: '/my-snoopy-app/',
+  base: '/snoopy-day/',
   plugins: [react()],
 })
